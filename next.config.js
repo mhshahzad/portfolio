a3f62d1529f848847dd343175369347972233756
Muhammad H. Shahzad <mh.shahzad@outlook.com>
2025-10-18 19:56:53 +0200

    chore: add GitHub Actions workflow for deployment to GitHub Pages and configure export settings

--- next.config.js
+++ next.config.js
@@ -2,7 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio/' : '',
   images: {
+    unoptimized: true,
     domains: ['images.unsplash.com'],
   },
   // Enable path aliases from tsconfig
